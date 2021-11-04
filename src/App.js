0250d7945836112d982437b6a510b587f518a850
Ian Hancock <northroadian@gmail.com>
2021-11-04 06:26:40 -0500

    conditionals

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css';
 
 
 function App() {
- const [showEvents, setShowEvents] = useState(true)
-  const [events,setEvents] = useState([
-    {title: "Ian loves Cake", id:1},
-    {title: "Ian eats the Cake", id:2},
-    {title: "Ian stomach is upset", id:3},
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    { title: "Ian loves Cake", id: 1 },
+    { title: "Ian eats the Cake", id: 2 },
+    { title: "Ian stomach is upset", id: 3 },
   ])
   const handleCLick = (id) => {
-      console.log(id)
- setEvents((prevEvents) => {
-   return prevEvents.filter((event)=>{
-     return id !== event.id
-   })
- })
- 
+    console.log(id)
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+
   }
 
   return (
     <div className="App">
-        <div>
+      {showEvents && (<div>
         <button onClick={() => setShowEvents(false)}>Hide Event</button>
-        </div>
-        <div>
+      </div>
+      )}
+      {!showEvents&&(
+      <div>
         <button onClick={() => setShowEvents(true)}>Show Event</button>
-      </div>
-    {showEvents && events.map((event, index)=>(
-      <div key={event.id}>
-        <h2>{index} - {event.title}</h2>
-        <button onClick={()=> handleCLick(event.id)}>delete event</button>
-      </div>
-    ))}
-    
+      </div>)}
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleCLick(event.id)}>delete event</button>
+        </div>
+      ))}
+
     </div>
 
   );
