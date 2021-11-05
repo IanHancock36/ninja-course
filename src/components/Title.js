b26d0599044c74e0cf732846f8f9e45a0b8d99c3
Ian Hancock <northroadian@gmail.com>
2021-11-05 06:00:31 -0500

    styling

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Title (){
+    return (
+        <div>
+       <h1 className='title'>Ian's Events </h1>
+       <br/>
+       <h2 className='subtitle'> The latest Events in my crazy Life</h2>
+        </div>
+    )
+}
+
